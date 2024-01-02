35f0cab04e48c0b75498b7c386fd9dd0ac468ac5
Vitalya <vitalyatihonov@mail.ru>
2024-01-02 22:54:08 +0900

    Initial Commit

---
+++ Keratin/KeratinApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeratinApp.swift
+//  Keratin
+//
+//  Created by пользователь on 02.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct KeratinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
