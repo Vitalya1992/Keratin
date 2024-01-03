be86e2c5418777dde8e57ed447becfaefdb4f255
Vitalya <vitalyatihonov@mail.ru>
2024-01-03 15:56:52 +0900

    new

---
+++ Keratin/Class.swift
@@ -0,0 +1,8 @@
+//
+//  Class.swift
+//  Keratin
+//
+//  Created by пользователь on 03.01.2024.
+//
+
+import Foundation
---
+++ Keratin/TodoList.swift
@@ -0,0 +1,8 @@
+//
+//  TodoList.swift
+//  Keratin
+//
+//  Created by пользователь on 03.01.2024.
+//
+
+import Foundation
